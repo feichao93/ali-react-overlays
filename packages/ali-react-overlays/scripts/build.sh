b3294b0f7d9a6b59e269488a11261fb87f4b249f
feichao.sfc <feichao.sfc@alibaba-inc.com>
2022-02-07 20:00:33 +0800

    fix: add styles in build

--- packages/ali-react-overlays/scripts/build.sh
+++ packages/ali-react-overlays/scripts/build.sh
@@ -4,3 +4,4 @@ yarn rollup --config rollup.config.js
 yarn api-extractor run --local --verbose
 yarn copyfiles --up 1 dist-temp/**/*.js dist/
 yarn rimraf dist-temp/
+yarn copyfiles --up 2 src/styles/* dist/styles/
